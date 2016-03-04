9b150e1974cdeb1e4297b06ee531d12d933061e9
David López <davidlghellin@gmail.com>
2016-03-04 14:20:18 +0100

    Subidos el resto de ficheros

---
+++ RepoOnline-PC/__openerp__.py
@@ -0,0 +1,12 @@
+{
+        'name':         'Apliaciones Programas desarrollados por Online-PC',
+        'description':  'Aplicacion de prueba de odoo',
+        'author':       'David López',
+        'depends':      ['base'],
+        'application':  'true',
+
+        'summary':      'Este es el summary',
+        'website':      'www.iesdealquerias.es',
+
+        'data':         ['online_view.xml'],
+}
